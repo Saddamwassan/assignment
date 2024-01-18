3f4b6669ef3576ed22e882a929d18c1d896b10af
SADDAM WASSAN <100377630+Saddamwassan@users.noreply.github.com>
2024-01-18 23:26:54 +0500

    assignment 18 jan 24

---
+++ script.js
@@ -0,0 +1,122 @@
+// counting *****************
+// for(var i=1;i<=10;i++){
+//     document.write(i);
+//     document.write("<br>");
+// }
+// counting-reverse**************
+// for(var i=10;i>=1;i--){
+//     document.write(i);
+//     document.write("<br>");
+// }
+// odd number******************
+// for(var i=1;i<=10;i++){
+//     if(i%2==1){
+//         document.write(i);
+//         document.write("<br>");
+//     }
+// }
+// even reverse **************
+// for(var i=10;i>=1;i--){
+//     if(i%2==0){
+//         document.write(i);
+//         document.write("<br>");
+//     }
+// }
+// star loop ****************
+// for(var i=1;i<=10;i++){
+//         document.write("*");
+//         document.write("<br>");
+//     }
+// star 2nd loop *************
+for(var i=1;i<=5;i++){
+    for(var j=1;j<=i;j++){
+        // for(var k=1;k<=1;k++){
+        // document.write(" ");
+        // }
+        document.write("*");
+
+    }
+    document.write("<br>")
+ 
+}
+// star 3rd loop**************
+// for(var i=1;i<=5;i++){
+//     for(var j=1;j<=i;j++){
+//     document.write("*");
+//     }
+//     document.write("<br>");
+// }
+// number triganle *****************
+// for(var i=1;i<=5;i++){
+//     for(var j=1;j<=i;j++){
+//         document.write(j);
+//     }
+//     document.write("<br>");
+// }
+// number triganle **************
+// for(var i=1;i<=5;i++){
+//     for(var j=0;j<=i;j++){
+//         document.write(j+i);
+//     }
+//     document.write("<br>");
+// }
+// even odd function **************
+// function number(a){
+//     if(a%2==0){
+//         document.write(a+" Even");
+//     }else{
+//         document.write(a+" Odd");
+//     }
+// }
+// number(9);
+// return reminder ***************
+// function number(a){
+//    var b = a%2;
+//    return b;
+// }
+// var c = number(7);
+// document.write(c);
+// program *******************
+// addition 
+// function add(a,b){
+//     var c = a+b;
+//     document.write(c);
+// }
+// add(12,33);
+// document.write("<br>");
+// subtraction***************
+// function sub(a,b){
+//     var c = a-b;
+//     document.write(c);
+// }
+// sub(12,33);
+// document.write("<br>");
+// Multiplication ************
+// function mul(a,b){
+//     var c = a*b;
+//     document.write(c);
+// }
+// mul(12,33);
+// document.write("<br>");
+// division *****************
+// function div(a,b){
+//     var c = a/b;
+//     document.write(c);
+// }
+// div(12,33);
+// document.write("<br>");
+// Exponent *****************
+// function exp(a,b){
+//     var c = a**b;
+//     document.write(c);
+// }
+// exp(5,3);
+// modulus **********************
+// function mod(a,b){
+//         var c = a%b;
+//         document.write(c);
+//     }
+//     mod(25,10);
+       
+
+
